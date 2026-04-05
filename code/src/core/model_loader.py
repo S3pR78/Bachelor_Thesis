de3fc1648ccaee60a5c3ff37865d9e11b63ba859
S3pR <s3moprojects.gmail.com>
2026-04-05 23:26:08 +0200

    feat(models): add local model and tokenizer loader

--- code/src/core/model_loader.py
+++ code/src/core/model_loader.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from src.core.download_manager import get_original_model_path
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 def get_model_dir(model_config: dict) -> Path:
     """
@@ -14,3 +15,22 @@ def get_model_dir(model_config: dict) -> Path:
     if not model_dir.exists():
         raise FileNotFoundError(f"Model directory not found: {model_dir}")
     return model_dir
+
+
+
+
+def load_model_and_tokenizer(model_config: dict):
+    """Loads the model and tokenizer based on the model configuration."""
+    model_dir = get_model_dir(model_config)
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_dir,
+        local_files_only=True,
+    )
+
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        model_dir,
+        local_files_only=True,
+    )
+
+    return tokenizer, model
